1c74a9cc6b6066ebf72d689e26cd606fcd333e75
Florian Hinterleitner <florian.hinterleitner@gmail.com>
2024-05-17 14:59:19 +0200

    task done

--- roman_numerals/roman_numerals.py
+++ roman_numerals/roman_numerals.py
@@ -1,69 +1,88 @@
-class RomanNumerals:
-    @staticmethod
-    def to_roman(val: int) -> str:
-        int_to_roman = {
-            1: 'I',
-            4: 'IX',
-            5: 'V',
-            9: 'IX',
-            10: 'X',
-            40: 'XL',
-            50: 'L',
-            90: 'XC',
-            100: 'C',
-            400: 'CD',
-            500: 'D',
-            900: 'CM',
-            1000: 'M'
-        }
+def to_roman(number: int) -> str:
+    int_to_roman = [
+        (1000, 'M'),
+        (900, 'CM'),
+        (500, 'D'),
+        (400, 'CD'),
+        (100, 'C'),
+        (90, 'XC'),
+        (50, 'L'),
+        (40, 'XL'),
+        (10, 'X'),
+        (9, 'IX'),
+        (5, 'V'),
+        (4, 'IV'),
+        (1, 'I')
+]
 
-        roman_num = []
+    roman_num = []
+
+    #iterate through conversion list
+    for (value, symbol) in int_to_roman:
+
+        #check if number is greater/equal to the value in the dictionary
+        while number >= value:
+            #add symbol to current roman number
+            roman_num.append(symbol)
+
+            #subtract value of symbol from integer number
+            number -= value
+
+    #join and return list values
+    return ''.join(roman_num)
+
+
+
+def from_roman(roman_num: str) -> int:
+    roman_to_int = {
+        'M': 1000,
+        'D': 500,
+        'C': 100,
+        'L': 50,
+        'X': 10,
+        'V': 5,
+        'I': 1,
+        'CM': 900,
+        'CD': 400,
+        'XC': 90,
+        'XL': 40,
+        'IX': 9,
+        'IV': 4
+    }
+
+    pos = 0
+    sum = 0
+
+    #iterate through the string
+    while pos < len(roman_num):
+        #check for multi character numbers
+        if pos+1 < len(roman_num) and roman_num[pos:pos+2] in roman_to_int:
+            #add multi character value to sum
+            sum += roman_to_int[roman_num[pos:pos+2]]
+
+            #increase position
+            pos += 2
+
+        else:
+            #add single character number to sum
+            sum += roman_to_int[roman_num[pos:pos+1]]
+
+            #increase position
+            pos += 1
+
+    return sum
 
 
 
 
 
 
-        return roman_num
+print(to_roman(1990))  # Output: 'MCMXC'
+print(to_roman(2008))  # Output: 'MMVIII'
+print(to_roman(1666))  # Output: 'MDCLXVI'
 
 
 
-    @staticmethod
-    def from_roman(roman_num: str) -> int:
-        roman_to_int = {
-            'M': 1000,
-            'D': 500,
-            'C': 100,
-            'L': 50,
-            'X': 10,
-            'V': 5,
-            'I': 1,
-            'CM': 900,
-            'CD': 400,
-            'XC': 90,
-            'XL': 40,
-            'IX': 9,
-            'IV': 4
-        }
-
-        pos = 0
-        sum = 0
-
-        #iterate through the string
-        while pos < len(roman_num):
-            #check for multi character numbers
-            if pos+1 < len(roman_num) and roman_num[pos:pos+2] in roman_to_int:
-                #add multi character value to sum
-                sum += roman_to_int[roman_num[pos:pos+2]]
-
-                #increase position
-                pos += 2
-
-            else:
-                #add single character number to sum
-                sum += roman_to_int[roman_num[pos:pos+1]]
-
-                #increase position
-                pos += 1
-
-        return sum
+print(from_roman('MCMXC'))  # Output: 1990
+print(from_roman('MMVIII')) # Output: 2008
+print(from_roman('MDCLXVI')) # Output: 1666
